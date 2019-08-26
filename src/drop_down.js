a2e26e2179f79467db895c9968f8f81bdadc8c82
Michelle Kim <x1michelleskim@gmail.com>
2019-08-26 09:56:40 -0700

    Create dogLinkCreator function

--- src/drop_down.js
+++ src/drop_down.js
@@ -9,3 +9,21 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+
+const dogLinkCreator = () => {
+  let dogsArr = [];
+
+  for(let i = 0; i < Object.keys(dogs).length; i++) {
+    let aTag = document.createElement('a');
+    aTag.innerHTML = Object.keys(dogs)[i];
+    aTag.setAttribute('href', Object.values(dogs)[i]);
+
+    let li = document.createElement('li');
+    li.classList.add('dog-link');
+    li.appendChild(aTag);
+
+    dogsArr.push(li);
+
+  }
+}
+
